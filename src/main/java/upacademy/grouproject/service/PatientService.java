e80b6a1545ec72be7b08f3fd23d17c9950b75bb9
PedroSilva <joao.pgja.silva@gmail.com>
2017-06-02 18:16:11 +0100

    first classes

---
+++ src/main/java/upacademy/grouproject/service/PatientService.java
@@ -0,0 +1,38 @@
+package upacademy.grouproject.service;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import upacademy.grouproject.model.Patient;
+
+@Named("patientService")
+@RequestScoped
+public class PatientService implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// unitName defined in persistence.xml
+	@PersistenceContext(name = "database")
+	static EntityManager em;
+
+	// Add patient to database
+	public void createPatient(Patient patient) {
+		em.persist(patient);
+	}
+
+	// Remove patient from database
+	public void removePatient(Long nHSCnumber) {
+		Patient patientremove = em.find(Patient.class, nHSCnumber);
+		em.remove(patientremove);
+	}
+	
+	// Edit patient merge to database
+	public void editPatient(Long nHSCnumber) {
+		Patient patientedit = em.find(Patient.class, nHSCnumber);
+		em.merge(patientedit);
+	}
+	
+}
