e0ed72e7cad108b96701822516b5b2a77e04cbbd
Not-Vincent <J_PP_Valentim@hotmail.com>
2017-06-20 19:43:18 +0100

    Patient(Create + List) actualizados

---
+++ Patient.java
@@ -0,0 +1,149 @@
+package upacademy.grouproject.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Patient extends EntityModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	// National Health Service Card Number
+	@Size(min=1, message = "Please insert a NHS card number")
+	private String nHS;
+	// Patient name
+	@Size(min =1, message = "Please insert a name")
+	private String name;
+	// Patient blood type
+	@NotNull(message = "Please insert a blood type")
+	private String bloodType;
+	// Patient date of birth
+	@Size(min =1, message = "Please insert a date of birth")
+	private String dateOfBirth;
+	@NotNull(message = "Please specify a gender")
+	private String gender;
+	// Patient phone (city)
+	@Size(min =1, message = "Please insert a phone number")
+	private String phone;
+	// Patient Email (city)
+	@Size(min =1, message = "Please insert a town hall")
+	private String email;
+	// Patient street
+	@Size(min =1, message = "Please insert a street")
+	private String street;
+	// Patient door number and floor
+	@Size(min =1, message = "Please insert a floor number")
+	private String numFloor;
+	// Patient zipCode
+	@Size(min =1, message = "Please insert ZIP code")
+	private String zipCode;
+	// Patient town
+	@Size(min =1, message = "Please insert a town")
+	private String town;
+	// Optional Observations; no validation required
+	private String observations;
+
+
+	// Getters & Setters
+	public String getnHS() {
+		return nHS;
+	}
+
+	public void setnHS(String nHS) {
+		this.nHS = nHS;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumFloor() {
+		return numFloor;
+	}
+
+	public void setNumFloor(String numFloor) {
+		this.numFloor = numFloor;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getTown() {
+		return town;
+	}
+
+	public void setTown(String town) {
+		this.town = town;
+	}
+
+	public String getObservations() {
+		return observations;
+	}
+
+	public void setObservations(String observations) {
+		this.observations = observations;
+	}
+
+}
---
+++ PatientService.java
@@ -0,0 +1,69 @@
+package upacademy.grouproject.service;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import upacademy.grouproject.model.Patient;
+import upacademy.grouproject.model.Triage;
+import upacademy.grouproject.repository.PatientRepository;
+import upacademy.grouproject.view.PatientBean;
+import upacademy.grouproject.view.TriageBean;
+
+@Named("patientService")
+@RequestScoped
+public class PatientService extends EntityService<Patient> {
+
+	@Inject
+	PatientRepository er = new PatientRepository();
+
+	@Inject
+	PatientBean patient;
+
+	// Add new Patient
+	public void newPatient(Patient patient) {
+		er.persistEntity(patient);
+		newBean();
+	}
+
+	// Edit patient
+	public void mergePatient(Long ID, Patient patient) {
+		// Find patient in the database
+		Patient emp = er.findEntity(Patient.class, ID);
+
+		emp.setnHS(patient.getnHS());
+		emp.setName(patient.getName());
+		emp.setBloodType(patient.getBloodType());
+		emp.setDateOfBirth(patient.getDateOfBirth());
+		emp.setGender(patient.getGender());
+		emp.setPhone(patient.getPhone());
+		emp.setEmail(patient.getEmail());
+		emp.setStreet(patient.getStreet());
+		emp.setNumFloor(patient.getNumFloor());
+		emp.setZipCode(patient.getZipCode());
+		emp.setTown(patient.getTown());
+		emp.setObservations(patient.getObservations());
+		
+		// Merge the new patient to the database
+		er.mergeEntity(emp);
+		newBean();
+	
+	}
+
+	// Check if patient exists
+	public boolean checkNHS(String nHS) {
+		if (er.checkIfExists(nHS).isEmpty() == true) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// New bean
+	public void newBean() {
+		this.patient.setPatient(new Patient());
+	}
+}
